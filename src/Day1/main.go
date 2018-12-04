468ffcd69207de9e49211772355f851bad28655f
Nicholas Richman <Nicholas.Richman@garmin.com>
2018-12-04 08:49:02 -0600

    Solved day 1.2
    Added the solution for puzzle 1.2.

--- src/Day1/main.go
+++ src/Day1/main.go
@@ -8,11 +8,11 @@ import (
 )
 
 func main() {
-	ProblemOne()
-	ProblemTwo()
+	fmt.Println(ProblemOne())
+	fmt.Println(ProblemTwo())
 }
 
-func ProblemOne() {
+func ProblemOne() int {
 	total := 0
 	data := utils.ReadFileAsStringSlice("input_1.txt")
 
@@ -31,8 +31,38 @@ func ProblemOne() {
 			total -= value
 		}
 	}
-	fmt.Println(total)
+	return total
 }
 
-func ProblemTwo() {
+func ProblemTwo() int {
+	total := 0
+	data := utils.ReadFileAsStringSlice("input_1.txt")
+	dict := map[int]int{}
+
+	loop := true
+	for loop {
+		for _, line := range data {
+			line := strings.Split(line, "")
+			value, err := strconv.Atoi(strings.Join(line[1:], ""))
+			if err != nil {
+				continue
+			}
+
+			if line[0] == "+" {
+				total += value
+			}
+
+			if line[0] == "-" {
+				total -= value
+			}
+
+			if _, ok := dict[total]; ok {
+				loop = false
+				break
+			} else {
+				dict[total] = 0
+			}
+		}
+	}
+	return total
 }
