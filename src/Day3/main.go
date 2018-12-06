86e5e096664cfae43cd6dd0657940101547a7ccc
Nicholas Richman <nrichman962@gmail.com>
2018-12-05 23:51:17 -0600

    Solved day 3.2
    Added the solution for problem 3.2.

--- src/Day3/main.go
+++ src/Day3/main.go
@@ -8,11 +8,12 @@ import (
 )
 
 func main() {
-	fmt.Println(problemOne())
-	//fmt.Println(problemTwo())
+	answer1, claims := problemOne()
+	fmt.Println(answer1)
+	fmt.Println(problemTwo(claims))
 }
 
-func problemOne() int {
+func problemOne() (int, [1000][1000]int) {
 	data := utils.ReadFileAsStringSlice("input_1.txt")
 	claims := [1000][1000]int{}
 	answer := 0
@@ -45,5 +46,37 @@ func problemOne() int {
 		}
 	}
 
-	return answer
+	return answer, claims
+}
+
+func problemTwo(claims [1000][1000]int) string {
+	data := utils.ReadFileAsStringSlice("input_1.txt")
+
+	for _, line := range data {
+		line := strings.Split(line, " ")
+
+		startX, _ := strconv.Atoi(strings.Split(line[2], ",")[0])
+		startYStr := strings.Split(line[2], ",")[1]
+		startY, _ := strconv.Atoi(startYStr[:len(startYStr)-1])
+
+		lengthX, _ := strconv.Atoi(strings.Split(line[3], "x")[0])
+		lengthY, _ := strconv.Atoi(strings.Split(line[3], "x")[1])
+
+		fmt.Printf("%d %d\n", startX, lengthX)
+		fmt.Printf("%d %d\n", startY, lengthY)
+
+		for i := startX; i < startX+lengthX; i++ {
+			for j := startY; j < startY+lengthY; j++ {
+				if claims[i][j] > 1 {
+					goto next
+				}
+			}
+		}
+
+		return line[0][1:]
+
+	next:
+		continue
+	}
+	return ""
 }
